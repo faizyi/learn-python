64a51c595d940740936dd85d5ff63f234ee10ab8
faizyi <faizi13062000@gmail.com>
2025-05-21 00:55:53 +0300

    set

---
+++ python part 4/set.py
@@ -0,0 +1,30 @@
+#immutable & unordered & dict & list are not allowed
+# collection = {1,2,2,3, "Hello", "World", 99, ("faiz")}
+# print(type(collection))
+# print(len(collection))
+# print(collection)
+
+# collection = set({1,2,3,3})
+# collection = "hello"
+# print(type(collection))
+# print(collection)
+
+#Methods
+# collection.add(3)
+# collection.add((4,5,6,7))
+# collection.remove(2)
+# collection.clear()
+# collection.pop()
+# print(len(collection))
+
+set1 = {1,2,3}
+set2 = {3, 4,5}
+# print(set1.union(set2))
+# print(set1)
+# print(set2)
+
+print(set1.intersection(set2))
+
+
+
+
